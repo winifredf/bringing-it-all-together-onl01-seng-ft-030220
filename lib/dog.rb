64cfb0fc9ebb2bf95b0af06aa53b49ac18c17f8e
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 09:10:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -63,7 +63,7 @@ class Dog
       sql = <<-SQL
         SELECT * FROM dogs WHERE id = ?
       SQL
-      DB[:conn]execute(sql, id).map do |row|
+      DB[:conn].execute(sql, id).map do |row|
         self.new_from_db(row)
     end.first
     
