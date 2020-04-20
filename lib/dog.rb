0bbac665e4b038f32dbc29efa04e132b2fb6c0ea
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 08:36:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,7 @@ class Dog
   attr_accessor :name, :breed
   attr_reader :id
   
-    def initialize(name, breed, id = nil)
+    def initialize(id: nil, name:, breed:)
       @id = id
       @name = name
       @breed = breed
