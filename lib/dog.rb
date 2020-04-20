e39d9095014f60842fc90d13955e73c55b35ffc4
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 08:57:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -41,6 +41,8 @@ class Dog
       
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
     end
+    self
+  end
     
     def self.create(name:, breed:)
       dog =dog.new(name, breed)
