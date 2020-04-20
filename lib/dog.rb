03dcf9cfe12cfb3d422e22da4f961b928a141906
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 08:55:36 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,6 +29,10 @@ class Dog
     end
     
     def save
+      if self.id
+        self.update
+      else
+        
       sql = <<-SQL
         INSERT INTO dogs (name, breed)
         VALUES (?, ?)
