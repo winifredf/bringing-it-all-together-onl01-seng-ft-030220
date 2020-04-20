73733c4d7dcf63e26be4eb1f225dc263d9dd7a6b
Winifred Frierson <winifred.dykes@hotmail.com>
2020-04-20 09:23:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,7 +68,7 @@ class Dog
       end.first
     end
     
-    def self.find_or_create_by(name, breed)
+    def self.find_or_create_by(name:, breed:)
       dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?, name, breed")
       if !dog.empty?
         dog_data = dog[0]
